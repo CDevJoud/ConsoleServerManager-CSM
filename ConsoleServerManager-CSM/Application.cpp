98fcc652b86f1af16c9c51ec7cecbd2f4e6831db
CDevJoud <97121061+CDevJoud@users.noreply.github.com>
2023-07-11 12:44:11 +0200

    tested panel class

--- ConsoleServerManager-CSM/Application.cpp
+++ ConsoleServerManager-CSM/Application.cpp
@@ -4,11 +4,15 @@ VOID Application::Run()
 {
 	this->InitConsoleWindow();
 	this->CreateConsoleBufferWindow(Vector2i(240, 64));
-	
+	Panel panel;
+	panel.CreatePanel(Vector2i(100, 32));
+	panel.SetPosition(Vector2i(240 / 2 - 50, 64 / 2 - 16));
 	while (this->IsInit())
 	{
-		this->ClearScreen();
+		this->ClearScreen(0x2588, 0x0B);
 
+		this->Render(panel);
+		
 		this->Display();
 	}
 }
