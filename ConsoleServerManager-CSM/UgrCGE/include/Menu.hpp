d93590052fc5b83b73461f7c7e87b20c3ae540bf
CDevJoud <97121061+CDevJoud@users.noreply.github.com>
2023-07-21 23:26:15 +0200

    Added Menu functionalities

--- ConsoleServerManager-CSM/UgrCGE/include/Menu.hpp
+++ ConsoleServerManager-CSM/UgrCGE/include/Menu.hpp
@@ -1,15 +1,22 @@
 #pragma once
 #include <Windows.h>
 #include <Vector.hpp>
+#include <vector>
 namespace ugr
 {
 	class Menu
 	{
 	public:
 		VOID CreateMenu(Vector2i size);
+		VOID AddElements(LPCWSTR str);
+		VOID SetColor(SHORT color);
 	private:
+		SHORT m_n16Color = 0x08;
+		BOOL m_bIsHidden = TRUE;
+		CHAR_INFO* m_MiniBuffer = nullptr;
+		std::vector<LPCWSTR> m_vecElements;
 		friend class Panel;
 		Vector2i m_ClickableMenuPosition;
-
+		Vector2i m_size;
 	};
 }
--- ConsoleServerManager-CSM/UgrCGE/src/Menu.cpp
+++ ConsoleServerManager-CSM/UgrCGE/src/Menu.cpp
@@ -4,6 +4,15 @@ namespace ugr
 {
 	VOID Menu::CreateMenu(Vector2i size)
 	{
-		
+		this->m_size = size;
+		//this->m_MiniBuffer = new CHAR_INFO[this->m_size.x * this->m_size.y];
+	}
+	VOID Menu::AddElements(LPCWSTR str)
+	{
+		this->m_vecElements.push_back(str);
+	}
+	VOID Menu::SetColor(SHORT color)
+	{
+		this->m_n16Color = color;
 	}
 }
